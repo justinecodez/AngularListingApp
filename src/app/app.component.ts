7671536ace0cfd17f300aba7479a94b1b574ee6e
justinepeterson <jmahinyila@gmail.com>
2019-04-04 18:34:20 +0300

    added a custome component called  customers with the command ngg c customers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({ 
   selector: 'app-root',
   template: `
-    <h1>Hello World</h1>
+    <h1>{{hello}}</h1>
   `
 })
 export class AppComponent implements OnInit {
-
+    hello: string;
   constructor() { }
 
   ngOnInit() {
+      this.hello = 'Hello world from databinding'
 
   }
   
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 //this is the main module it organises all the code of the app
 import { AppComponent }  from './app.component';
+import { CustomersComponent } from './customers/customers.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, CustomersComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
